6f62744ad57b0c0758fef2082828294c6552761d
Abhay Patel <patel.abhay1@hotmail.com>
2020-09-30 15:02:15 -0400

    Updates

---
+++ precache-manifest.4f4b4f305561726bb9059fb352f0fe95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004ecbe1de644755172b71364c475d5a",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "815cc0cbd193960dabb7",
+    "url": "/TicTacToe/static/css/main.a9157afc.chunk.css"
+  },
+  {
+    "revision": "08dcb03beea7e1b098c8",
+    "url": "/TicTacToe/static/js/2.8aa62060.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.8aa62060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815cc0cbd193960dabb7",
+    "url": "/TicTacToe/static/js/main.f2b0b10d.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  }
+]);
